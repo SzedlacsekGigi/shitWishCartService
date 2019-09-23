3875378724fe76808db39b50d1a04578f6f9cd6c
SzedlacsekGigi <szedlacsek.lilla@gmail.com>
2019-09-23 13:02:39 +0200

    CartController added, with get mapping to show all cart items

---
+++ src/main/java/com/codecool/cartservice/controller/CartController.java
@@ -0,0 +1,25 @@
+package com.codecool.cartservice.controller;
+
+import com.codecool.cartservice.CartService;
+import com.codecool.cartservice.model.CartIem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cart")
+@CrossOrigin
+public class CartController {
+
+    @Autowired
+    CartService cartService;
+
+    @GetMapping("/all")
+    public List<CartIem> getAllCartItems() {
+        return cartService.retrieveAll();
+    }
+}
